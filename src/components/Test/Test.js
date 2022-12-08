dca42704227002fb0fdd3a28195d6890ed7078af
mainanchung <melodychung522@gmail.com>
2022-12-08 04:13:05 -0500

    questions setting done

--- src/components/Test/Test.js
+++ src/components/Test/Test.js
@@ -1,28 +1,46 @@
 import  './Test.scss'
-import test1 from '../../data/test.json'
+import { useState } from 'react';
 
-function Test(){
-   const allTests = test1
-   console.log(allTests.map(quiz => quiz.options[0].value))
-return(
-        <section className='test'> 
-            {allTests.map(quiz => <>
-                    <div className='test__single'>
+
+
+function Test({quiz, class_name}){
+
+   const allTests = quiz
+   const [answers, setAnswers] = useState([])
+   const handleChange = (e) => {
+    e.preventdefault()
+  }
+
+    return(
+        <form className={class_name} id={class_name}> 
+            {allTests.map(quiz => 
+                    <div className='test__single' key={quiz.id}>
                         <h2>{quiz.question}</h2>
                             <div className='test__select'>
-                                <input type="radio" name={quiz.id} value={quiz.options[0].value}/>
+                                <input 
+                                type="radio" 
+                                name={`question${quiz.id}`} 
+                                value={quiz.options[0].value}
+                                onChange
+                                checked
+                                />
                                 <label>{quiz.options[0].choice}</label>                           
                             </div>
                             <div className='test__select'>
-                                <input type="radio" name={quiz.id} value={quiz.options[1].value}/>
+                                <input 
+                                type="radio" 
+                                name={`question${quiz.id}`} 
+                                value={quiz.options[1].value}
+                                onChange
+                                checked/>
                                 <label>{quiz.options[1].choice} </label>                           
                             </div>
                     </div>
-                </>
+                
             )}
-    </section>
+          <button type="submit" onSubmit={handleChange}> NEXT </button>
+    </form>
     )          
 }
-
 
 export default Test;
--- src/pages/Test/TestPage.js
+++ src/pages/Test/TestPage.js
@@ -1,14 +1,16 @@
 import './TestPage.scss'
-import TestBank from '../../data/test.json' 
+import Test1 from '../../data/test1.json'
+import Test2 from '../../data/test2.json'
+import Test3 from '../../data/test3.json'
+import Test4 from '../../data/test4.json'  
 import Test from '../../components/Test/Test';
 import { useNavigate, NavLink, useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 
 
 function TestPage(){
-    // const navigate = useNavigate()
-    // const goNextHandler = () =>{    
-    // }
+    const [currentScore, setCurrentscore] = useState([]);
+
     return( 
         <>      
         <div className='hero'>
@@ -19,7 +21,26 @@ function TestPage(){
              Identifying a career path based on what you truly are!</p>
              <button className='test__start-btn' >Let's start !!</button>
         </div>
-        <Test/>
+            <Test
+            key={Test1.id}  
+            quiz={Test1}
+            class_name="test1"
+            />
+            <Test 
+            key={Test2.id} 
+            quiz={Test2}
+            class_name="test2"
+            />
+            <Test  
+            key={Test3.id}
+            quiz={Test3}
+            class_name="test3"
+            />
+            <Test 
+            key={Test4.id} 
+            quiz={Test4}
+            class_name="test4class_name"
+            />
         </> 
     )
 }
