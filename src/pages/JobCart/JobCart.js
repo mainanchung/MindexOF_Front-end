8ee1ffc0b133f625a226073960993489b76cd080
mainanchung <melodychung522@gmail.com>
2022-12-16 06:20:54 -0500

    responsive design

--- src/pages/JobCart/JobCart.js
+++ src/pages/JobCart/JobCart.js
@@ -22,11 +22,11 @@ function JobCart({jobCart,setJobCart}){
 
     return( 
         <> 
+        <div className='cart'>
         <Header 
         jobCart={jobCart}
         setJobCart={setJobCart}
         />
-        <div className='cart'>
                 <div className='cart__container'>
                 <h1  className='cart__title'>My Jobs    
                 {jobCart.length?<span> ({jobCart.length}) </span> :""}
@@ -35,9 +35,11 @@ function JobCart({jobCart,setJobCart}){
                         {jobCart.length?
                         jobCart.map(item =>
                             <div className='cart-card'>
-                            <h2 className='cart-card__title'>{item.title}</h2>
-                                        <p className='cart-card__info'>{item.company.display_name}</p>
-                                        <p className='cart-card__info'>{item.location.area[1]}  {item.location.area[0]}</p>             
+                            <div className='cart-card__content'>
+                                <h2 className='cart-card__title'>{item.title}</h2>
+                                <p className='cart-card__info'>{item.company.display_name}</p>
+                                <p className='cart-card__info'>{item.location.area[1]}  {item.location.area[0]}</p>  
+                            </div>           
                               <div className='cart-card__function'>
                                 <img src={deletIcon} alt='delete-icon'  onClick={() => deleteJob(item.id)} className='cart-card__delete'/>
                                 <a className="cart-card__url" href={item.redirect_url}> <button className='cart-card__btn'>Apply</button></a>
