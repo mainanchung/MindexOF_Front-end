0e50578bd5f45d58f7d80b3b8cab2596a67aa89f
mainanchung <melodychung522@gmail.com>
2023-01-07 03:18:01 -0500

    search bar optimizing

--- src/components/JobTicket/JobTicket.js
+++ src/components/JobTicket/JobTicket.js
@@ -7,6 +7,7 @@ const getJobCartFromLocalStorage = JSON.parse(localStorage.getItem("cart")|| "[]
 
 function JobTicket({job, setJobCart, jobCart}){
 
+
     const  SalaryFormatter= (num) =>{
         return Math.abs(num) > 999 ? Math.sign(num)*((Math.abs(num)/1000).toFixed(1)) + 'k' : Math.sign(num)*Math.abs(num)
     }
